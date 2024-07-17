f57612dca533877efec7ea77249703e5719ee47f
jC5JYepmkS2q3SxhK7mN <171768122+jC5JYepmkS2q3SxhK7mN@users.noreply.github.com>
2024-07-18 02:26:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '210126fb-c71f-4951-a929-89b03f8e636e';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
